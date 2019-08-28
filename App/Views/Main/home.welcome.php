968c54d698ceda3e80c28ac68e0a3ae417cc7613
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2019-08-28 17:52:16 +0600

    Replace ? with URL_SEPARATOR

--- App/Views/Main/home.welcome.php
+++ App/Views/Main/home.welcome.php
@@ -91,7 +91,7 @@ if(!isset($title)) $title = \ADACT\Config::SITE_TITLE;
                         <i class="fa fa-user-plus" aria-hidden="true"></i> Register
                     </a>
                 </div>
-                <div class="palegoldenrod">Or, <a href="./login?guest=true" style="color: palegoldenrod; text-decoration: underline;">try it without login</a></div>
+                <div class="palegoldenrod">Or, <a href="<?php echo './login'. URL_SEPARATOR .'guest=true' ?>" style="color: palegoldenrod; text-decoration: underline;">try it without login</a></div>
             </div>
         </div>
     </div>
